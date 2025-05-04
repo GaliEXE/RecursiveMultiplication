6d9bbbc806651c399f978f8237973e6f81d6a6bd
Galiexe <gryphon.c.miller@gmail.com>
2025-05-04 15:05:56 -0500

    Added Corrections

--- src/recursiveMultiplication/RecursiveMultiplication.java
+++ src/recursiveMultiplication/RecursiveMultiplication.java
@@ -8,30 +8,31 @@ public class RecursiveMultiplication {
 	static double product;
 	
 	public static void main(String[] args) {
-		Scanner scnr = new Scanner(System.in);
-		try {
+		try (Scanner scnr = new Scanner(System.in)){
+			double[] numbers = new double[5];
 			System.out.println("Please Enter 5 Numbers To Be Multiplied Together!");
-			System.out.println("First Number: ");
-			double number1 = scnr.nextDouble();
-			System.out.println("Second Number: ");
-			double number2 = scnr.nextDouble();
-			System.out.println("Third Number: ");
-			double number3 = scnr.nextDouble();
-			System.out.println("Fourth Number: ");
-			double number4 = scnr.nextDouble();
-			System.out.println("Fifth Number: ");
-			double number5 = scnr.nextDouble();
-			scnr.close();
 			
-			double[] numbers = {number1, number2, number3, number4, number5};
+			for(int i = 0; i < numbers.length; i++) {
+				while (true) {
+					System.out.printf("Number %d: ", i+ 1);
+					if(scnr.hasNextDouble()) {
+						numbers[i] = scnr.nextDouble();
+						break;
+					} else {
+						System.out.println("Invalid Input! Please Enter A Valid Number");
+						scnr.next();
+					}
+				}
+			}
 			
 			double finalProduct = multiply(numbers, 0);
-			
 			System.out.println(finalProduct);
-		} catch (InputMismatchException e) {
-			System.out.println("Invalid Input Please Restart The Program And Try Again!");
+			
+		} catch (Exception e) {
+			System.out.println("There Was An Error Refresh And Try Again! " + e);
 		}
 	}
+	
 	
 	public static double multiply(double[] numbers, int i) {
 		if (i >= numbers.length) {
